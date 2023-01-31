a00d2794cdf7997aa1826b7f8676ead3e9c16c09
Santino Valenzuela <sev@storis.com>
2023-01-31 10:15:13 -0500

    Create IVehicle interface and be able to inherit it from a child class

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -13,7 +13,45 @@ namespace TestProject
 
             Console.WriteLine("Test Project App");
 
+            Car car = new Car();
+            car.Start();
+
+            LawnMower lawnMower = new LawnMower();
+            lawnMower.Stop();
+
             Console.ReadLine();
+        }
+    }
+
+    interface IVehicle
+    {
+        void Start();
+        void Stop();
+    }
+
+    public class Car : IVehicle
+    {
+        public void Start()
+        {
+            Console.WriteLine("Car has started");
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("Car has stopped");
+        }
+    }
+
+    public class LawnMower : IVehicle
+    {
+        public void Start()
+        {
+            Console.WriteLine("Lawn Mower has started");
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("Lawn Mower has stopped");
         }
     }
 }
